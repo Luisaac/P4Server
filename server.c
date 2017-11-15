a9521f57deb1f7702d02343c3760ff83b1fba6fa
Zhenda Lu <zhenda@snares-08.cs.wisc.edu>
2017-11-15 14:26:54 -0600

    producer/consumer model

--- server.c
+++ server.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include "cs537.h"
 #include "request.h"
 
@@ -12,32 +13,64 @@
 //
 
 // CS537: Parse the new arguments too
-void getargs(int *port, int argc, char *argv[])
+void getargs(int *port, int argc, char *argv[], int *threads, int *buffers)
 {
-    if (argc != 2) {
-	fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+    if (argc != 4) {
+	fprintf(stderr, "Usage: %s <port> <threads> <buffers>\n", argv[0]);
 	exit(1);
     }
     *port = atoi(argv[1]);
+    *threads = atoi(argv[2]);
+    *buffers = atoi(argv[3]);
 }
 
 
 int main(int argc, char *argv[])
 {
+    pthread_cond_t empty, fill;
+
+    pthread_mutex_t* mutex;
+    pthread_mutexattr_t mutexAttribute;
+
     int listenfd, connfd, port, clientlen;
     struct sockaddr_in clientaddr;
 
-    getargs(&port, argc, argv);
+    int threads;
+    int buffers;
+    getargs(&port, argc, argv, &threads, &buffers);
+    int buffer[buffers];
+    int fill_ptr = 0;
+    int use_ptr = 0;
+    int count = 0;
 
     // 
     // CS537: Create some threads...
     //
 
-    listenfd = Open_listenfd(port);
-    while (1) {
-	clientlen = sizeof(clientaddr);
-	connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+    mutex = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+    pthread_mutexattr_init(&mutexAttribute);
+    pthread_mutexattr_setpshared(&mutexAttribute, PTHREAD_PROCESS_SHARED);
+    pthread_mutex_init(mutex, &mutexAttribute);
 
+
+    listenfd = Open_listenfd(port);
+//    while (1) {
+    // client/producer
+    for (int i = 0; i < threads; i++) {
+        pthread_mutex_lock(mutex);
+	    while (count == buffers)
+            pthread_cond_wait(&empty, mutex);
+
+        // put
+        clientlen = sizeof(clientaddr);
+	    connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+        buffer[fill_ptr] = connfd;
+        fill_ptr = (fill_ptr +1) % buffers;
+        count++; 
+        
+        pthread_cond_signal(&fill);
+        pthread_mutex_unlock(mutex);
+    }
 	// 
 	// CS537: In general, don't handle the request in the main thread.
 	// Save the relevant info in a buffer and have one of the worker threads 
@@ -45,10 +78,21 @@ int main(int argc, char *argv[])
 	// 
 
     // handle request after the lock
-	requestHandle(connfd);
+    while (1) {
+        pthread_mutex_lock(mutex);
+        while (count == 0) 
+            pthread_cond_wait(&fill, mutex);
+        int connfd = buffer[use_ptr];
+        use_ptr = (use_ptr + 1) % buffers;
+        count--;
+        pthread_cond_signal(&empty);
+        pthread_mutex_unlock(mutex);
+        requestHandle(connfd);
+    }
+	
 
 	Close(connfd);
-    }
+ //   }
 
 }
 
